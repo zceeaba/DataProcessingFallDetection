36f45b7aa939ba99fec088efe5a02b6bc9cecd7e
Abhishekh Jay baskaran <zceeaba@ucl.ac.uk>
2018-05-04 01:08:55 +0100

    video class

---
+++ FallDetect/video.py
@@ -0,0 +1,7 @@
+class video():
+    def __init__(self,twodbb,threedbb,twodcen,threedcen,silhouette):
+        self.twodboundingbox=twodbb
+        self.threedboundingbox=threedbb
+        self.twodcentre=twodcen
+        self.threedcentre=threedcen
+        self.silhouette=silhouette
